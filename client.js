07be8de751cfe1a8a2235b16f71f44855e55e3f8
omarchaeopteryx <omaralimalik@gmail.com>
2017-01-08 16:31:37 -0800

    Refactor my personal page

--- client.js
+++ client.js
@@ -1,3 +1,7 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log( "Hello again, World!" );
+
+    $( "#my-greeting" ).fadeIn( "slow", "linear" );
+    $( "#my-name" ).fadeIn( "slow", "linear" );
+});
 });
