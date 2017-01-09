f50eab8362c9c4250b9af10136421f626ab27d9e
omarchaeopteryx <omaralimalik@gmail.com>
2017-01-09 14:53:35 -0800

    Fix upper, left, right, lower display hidings; delete old images

--- client.js
+++ client.js
@@ -1,59 +1,95 @@
-$( document ).ready(function() {
-    console.log( "Hello again, World!" );
     var coordinateTracker = [0,0];
-    var centerBox = $(this).find("#center-box");
+$( document ).ready(function() {
 
-    function checkCenter() {
-      console.log(coordinateTracker[0]===0 && coordinateTracker[1]===0)
+    // Setting up website methods:
+
+    var centerCanvas = $(this).find("#canvas");
+
+    function checkCenter(coordinateTracker) {
+
+      var centerWindow = "";
+      if (coordinateTracker[0]===0 && coordinateTracker[1]===0) {
+        centerWindow = "#center-box"
+      } else if (coordinateTracker[0]>= 0 && coordinateTracker[1]===0) {
+        centerWindow = "#right-box"
+      } else if (coordinateTracker[0]<= 0 && coordinateTracker[1]===0) {
+      centerWindow = "#left-box"
+      } else if (coordinateTracker[0]=== 0 && coordinateTracker[1]>=0) {
+        centerWindow = "#upper-box"
+      } else if (coordinateTracker[0]=== 0 && coordinateTracker[1]<=0) {
+        centerWindow = "#lower-box"
+      }
+      console.log("you changed to... " + centerWindow)
+      return centerWindow
+
     }
+
 
     // Use this area to make mobile functionality:
 
-    $("#center-box").on("swipeleft",function(){
-      coordinateTracker[0] += 1;
-      console.log(coordinateTracker);
-      var newItem = $("#my-contact").show();
-      centerBox.html(newItem);
-    });
+    // $("#center-box").on("swipeleft",function(){
+    //   coordinateTracker[0] += 1;
+    //   console.log(coordinateTracker);
+    //   var newItem = $("#right-box").show();
+    //   centerCanvas.html(newItem);
+    // });
 
     // Use this area to make desktop functionality:
 
     $(document).keydown(function(e) {
 
+      // e.preventDefault(); // prevent the default action (scroll / move caret)
+
       switch(e.which) {
+
           case 37: // left
           console.log("You clicked to go left!")
+          var current = checkCenter(coordinateTracker);
+          $("#canvas").find(current).hide();
           coordinateTracker[0] -= 1; // Changing the x-coordinate.
-          console.log(coordinateTracker);
-                checkCenter();
+          var replacement = checkCenter(coordinateTracker);
+          $("#canvas").find(replacement).show();
+          return coordinateTracker
           break;
 
           case 38: // up
           console.log("You clicked to go up!")
+          var current = checkCenter(coordinateTracker);
+          $("#canvas").find(current).hide();
           coordinateTracker[1] += 1; // Changing the y-coordinate.
-          console.log(coordinateTracker);
-                checkCenter();
+          // console.log(coordinateTracker);
+          var replacement = checkCenter(coordinateTracker);
+          $("#canvas").find(replacement).show();
+          return coordinateTracker
           break;
 
           case 39: // right
           console.log("You clicked to go right!")
+          var current = checkCenter(coordinateTracker);
+          $("#canvas").find(current).hide();
           coordinateTracker[0] += 1;
-          console.log(coordinateTracker);
-          var newItem = $("#my-contact").show();
-          centerBox.html(newItem);
-                checkCenter();
+          var replacement = checkCenter(coordinateTracker);
+          $("#canvas").find(replacement).show();
+          return coordinateTracker
           break;
 
           case 40: // down
           console.log("You clicked to go down!")
+          var current = checkCenter(coordinateTracker);
+          $("#canvas").find(current).hide();
           coordinateTracker[1] -= 1;
-          console.log(coordinateTracker);
-                checkCenter();
+          var replacement = checkCenter(coordinateTracker);
+          $("#canvas").find(replacement).show();
+          return coordinateTracker
           break;
 
-          default: return; // exit this handler for other keys
+          default: return coordinateTracker; // exit this handler for other keys
+
+
       }
-      e.preventDefault(); // prevent the default action (scroll / move caret)
+
+
+
     });
 
 
