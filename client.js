96dd6ede4472fd5310ad0096069673342d68d4bb
omarchaeopteryx <omaralimalik@gmail.com>
2017-03-13 20:02:07 -0700

    Change font styling

--- client.js
+++ client.js
@@ -1,5 +1,24 @@
 var coordinateTracker = [0,0];
 
+// New: Add dynamic scrolling on My Story page.
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".story-text").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+
 $( document ).ready(function() {
 
     // Setting up website methods:
@@ -174,25 +193,5 @@ $( document ).ready(function() {
       replaceCurrent();
 
     });
-
-    // New: Add dynamic scrolling on My Story page.
-
-    $(window).on("load",function() {
-      $(window).scroll(function() {
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        $(".story-text").each(function() {
-          /* Check the location of each desired element */
-          var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-          /* If the element is completely within bounds of the window, fade it in */
-          if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-            if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-          } else { //object goes out of view (scrolling up)
-            if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-          }
-        });
-      }).scroll(); //invoke scroll-handler on page-load
-    });
-
 
 });
